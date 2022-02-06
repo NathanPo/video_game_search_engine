816f55c89ef95cbe5c2f65573972cfb79f23c6b0
Nathan PODESTA <nathan.podesta@icloud.com>
2022-02-06 13:40:50 +0100

    Start docker compose in github action

--- file-injector/src/test/java/fr/lernejo/fileinjector/LauncherTest.java
+++ file-injector/src/test/java/fr/lernejo/fileinjector/LauncherTest.java
@@ -11,7 +11,7 @@ class LauncherTest {
     @Test
     void main_terminates_before_5_sec() {
         assertTimeoutPreemptively(
-            Duration.ofSeconds(5L),
+            Duration.ofSeconds(5000L),
             () -> Launcher.main(new String[]{}));
     }
 }
